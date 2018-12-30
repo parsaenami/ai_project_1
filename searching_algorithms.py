b6f9e3e84b040af68b6e6da4faab770bb225a6a4
Parsa Enami <parsaenami@aut.ac.ir>
2018-12-30 14:57:56 +0330

    dfs

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -5,3 +5,9 @@ class ClassicSearchAlgorithm(object):
         self.parent = {}
         self.parent_from_goal = {}
         self.memory = 0
+
+    def graph_depth_first_search(self, start_state):
+        visited_nodes = []
+        nodes_to_expand = [start_state]
+        number_of_visited_nodes = 1
+        number_of_expanded_nodes = 0
