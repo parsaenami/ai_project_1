178e6d9098881e5da814cdb98910bcdf4c506609
Parsa Enami <parsaenami@aut.ac.ir>
2018-12-31 03:34:03 +0330

    graph class added

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -1,3 +1,18 @@
+from collections import defaultdict
+
+
+class Graph:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.nodes = []
+        self.edges = []
+
+    def add_edge(self, node1, node2, cost):
+        self.graph[node1].append((node2, cost))
+        self.graph[node2].append((node1, cost))
+
+
 class ClassicSearchAlgorithm(object):
 
     def __init__(self, problem):
